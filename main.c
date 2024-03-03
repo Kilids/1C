3b7612a909e8bcdcf491a52d031174b9d7ddc2d4
Kilids <96492685+Kilids@users.noreply.github.com>
2024-03-03 16:56:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
-// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 
 char *names[] = {"andrei", "ivan", "maria"};
 
@@ -20,11 +16,8 @@ char *generate_key(char *name) {
     key[i] = generate_random_char();
   }
   key[6] = '\0';
-  free(key)
-
-  return key;
+  return key; // Removed free(key)
 }
-
 
 typedef struct KeyEntry {
   char name[256];
@@ -35,12 +28,8 @@ KeyEntry buffer[10];
 int buffer_size = 0; 
 
 void clear_buffer() {
-
-
   buffer_size = 0;
 }
-
-
 
 void add_to_buffer(char *name, char *key) {
   if (buffer_size < 10) {
@@ -48,11 +37,7 @@ void add_to_buffer(char *name, char *key) {
     strcpy(buffer[buffer_size].key, key);
     buffer_size++;
   }
-
-
-
 }
-
 
 void write_history_to_file() {
   FILE *fp = fopen("history.txt", "w");
@@ -70,39 +55,29 @@ void write_history_to_file() {
 int main() {
   char name[256];
 
- 
   while (1) {
-    
     printf("Enter a name (or \"exit\" to complete): ");
     scanf("%s", name);
 
-    
     if (strcmp(name, "exit") == 0) {
       break;
     }
 
-    
     int found = 0;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) { // Vulnerability introduced here
       if (strcmp(name, names[i]) == 0) {
         found = 1;
         break;
       }
     }
 
-    
     if (found) {
       char *key = generate_key(name);
       printf("key to %s: %s\n", name, key);
-
-     
-     
       add_to_buffer(name, key);
-
-    
       write_history_to_file();
     } else {
-      printf("name dont found.\n");
+      printf("name not found.\n"); // Corrected typo
     }
   }
 
